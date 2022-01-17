54d15687d99b545cc0b490c47f78b107bb66ca45
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2022-01-16 22:09:12 -0600

    changed key

--- src/routes/checkout.ts
+++ src/routes/checkout.ts
@@ -1,5 +1,6 @@
 const router = require("express").Router();
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
+const stripe = require("stripe")("sk_test_51KIK8RIPi9H1voJKYJHfTfxt84MtLbqMK2wUpk768cwrywSdohQjOCoASKcX2rp6QE53hrJw9gutWxxbxANfcldH001EamFqeK");
 
 router.post("/payment", (req: any, res: any) => {
     stripe.charges.create(
@@ -15,7 +16,7 @@ router.post("/payment", (req: any, res: any) => {
                 res.status(200).json(stripeRes);
             }
         }
-    );
+    );    
 });
 
 module.exports = router;
