f36e0b2390a39d58c6df9eab2aee612bedd2dcce
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2022-01-24 19:03:50 -0600

    added dist directory

---
+++ dist/src/index.js
@@ -0,0 +1,35 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const cors_1 = __importDefault(require("cors"));
+const authRouter = require("./routes/auth");
+const productsRouter = require("./routes/product");
+const userRouter = require('./routes/user');
+const cartRouter = require('./routes/cart');
+const orderRouter = require('./routes/order');
+const checkoutRouter = require('./routes/checkout');
+const app = (0, express_1.default)();
+dotenv_1.default.config();
+mongoose_1.default
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("DB Connection Successfull!"))
+    .catch((err) => {
+    console.log(err);
+});
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartRouter);
+app.use("/api/orders", orderRouter);
+app.use("/api/checkout", checkoutRouter);
+app.listen(process.env.PORT || 5000, () => {
+    console.log('Backend Server is running!');
+});
+//# sourceMappingURL=index.js.map
---
+++ dist/src/models/Product.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const ProductSchema = new mongoose_1.default.Schema({
+    title: { type: String, required: true, unique: true },
+    desc: { type: String, required: true, },
+    img: { type: String, required: true },
+    categories: { type: Array },
+    size: { type: Array },
+    color: { type: Array },
+    price: { type: Number, required: true },
+    inStock: { type: Boolean, default: true }
+}, { timestamps: true });
+module.exports = mongoose_1.default.model("Product", ProductSchema);
+//# sourceMappingURL=Product.js.map
