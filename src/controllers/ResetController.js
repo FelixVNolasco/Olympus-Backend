1a6546591a87633ff8a7ae905ead5babd95c1416
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2022-09-21 17:04:15 -0500

    fixed client url env variable

--- src/controllers/ResetController.js
+++ src/controllers/ResetController.js
@@ -4,19 +4,27 @@ const {
 } = require("../services/AuthService");
 
 const resetPasswordRequestController = async (req, res, next) => {
-  const requestPasswordResetService = await requestPasswordReset(
-    req.body.email
-  );
-  return res.json(requestPasswordResetService);
+  try {
+    const requestPasswordResetService = await requestPasswordReset(
+      req.body.email
+    );
+    return res.status(200).json(requestPasswordResetService);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 };
 
 const resetPasswordController = async (req, res, next) => {
-  const resetPasswordService = await resetPassword(
-    req.body.userId,
-    req.body.token,
-    req.body.password
-  );
-  return res.json(resetPasswordService);
+  try {
+    const resetPasswordService = await resetPassword(
+      req.body.userId,
+      req.body.token,
+      req.body.password
+    );
+    return res.status(200).json(resetPasswordService);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 };
 
 module.exports = {
