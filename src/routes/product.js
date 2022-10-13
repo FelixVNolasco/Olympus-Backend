9faa829439ae8b92dd273a532abf121eea459ea4
Felix Enrique Vega Nolasco <49852681+FelixVNolasco@users.noreply.github.com>
2022-10-13 17:58:45 -0500

    returned _id in the $project state

--- src/routes/product.js
+++ src/routes/product.js
@@ -102,7 +102,7 @@ router.get("/search", async (req, res) => {
       },
       {
         $project: {
-          _id: 0,
+          _id: 1,
           title: 1,
           desc: 1,
           price: 1,
